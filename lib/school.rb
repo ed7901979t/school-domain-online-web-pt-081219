5940ae15df1c8a45182d8e951d063851ca5c8c76
clean-command-1851 <ed7901979t@gmail.com>
2019-08-24 20:20:37 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,7 +1,21 @@
 # code here!
 class School  
   
-  def school(schools_name)
+  def initialize(school)
+    @school=school
+  end
+  
+  def school_name(school)
+    @school=school
+  end
+  
+  def school_name
+    @school=school
+  end
+  
+  school = School.new("Bayside High School")
+  
+  roaster = {} #hash keys grade levels 
 
 
 
