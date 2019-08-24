55511c0abe8efdb5abf1914fedbcb0005e36df27
clean-command-1851 <ed7901979t@gmail.com>
2019-08-24 21:01:33 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,34 +1,13 @@
 # code here!
-class School  
+class School
+  attr_accessor :name, :roster
   
-  def initialize(school)
-    @school=school
-  end
-  
-  def school_name(school)
-    @school=school
-  end
-  
-  def school_name
-    @school=school
-  end
-  
-  school = School.new("Bayside High School")
-  
-  roaster = {} #hash keys grade levels and values for student names
-  
-  def school.add_student(names, grade)
-    roaster = {}
-    roaster.each do {|names, grade| 
-      roaster << names
-      roaster << grade
-    end
-    return roaster
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end 
   
   
-  
-  end
-
 
 
 
